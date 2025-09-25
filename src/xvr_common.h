e88f98de751012259c1c8e3ae5f8e332a292b567
slowy07 <slowy.arfy@proton.me>
2025-09-26 00:41:57 +0700

    chore: update platform supports
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- src/xvr_common.h
+++ src/xvr_common.h
@@ -25,41 +25,47 @@ SOFTWARE.
 #ifndef COMMON_XVR_TYPES_H
 #define COMMON_XVR_TYPES_H
 
-#if defined(__GNUC__)
+#if defined(__linux__)
 #define XVR_API extern
-#elif defined(_MSC_VER)
-#ifndef XVR_EXPORT
+#elif defined(_WIN32) || defined(_WIN64)
+#if defined(XVR_EXPORT)
+#define XVR_API __declspec(dellexport)
+#elif defined(XVR_IMPORT)
 #define XVR_API __declspec(dllimport)
 #else
-#define XVR_API __declspec(dllexport)
-#endif // !XVR_EXPORT
+#define XVR_API extern
+#endif // defined(XVR_EXPORT)
+#elif defined(__APPLE__)
+#define XVR_API extern
+#endif // defined(__linux__)
 
-#else
-#define XVRXVR_API extern
-
-#endif // defined(__GNUC__)
-
-#if defined(__GNUC__)
-#if defined(__x86_64)
+#if defined(__linux__)
+#if defined(__LP64__)
 #define XVR_BITNESS 64
 #else
 #define XVR_BITNESS 32
-#endif // defined(__x86_64)
-#elif defined(_MSC_VER)
-#if defined(_M_X64)
+#endif // defined(__LP64__)
+#elif defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN64)
 #define XVR_BITNESS 64
 #else
 #define XVR_BITNESS 32
-#endif // defined(_M_X64)
+#endif // defined(_WIN64)
+#elif defined(__APPLE__)
+#if defined(__LP64__)
+a#define XVR_BITNESS 64
+#else
+#define XVR_BITNESS 32
+#endif // defined(__LP64__)
 #else
 #define XVR_BITNESS -1
-#endif // defined(__GNUC__)
+#endif // defined(__linux__)
 
 #define XVR_VERSION_MAJOR 0
 #define XVR_VERSION_MINOR 1
 #define XVR_VERSION_PATCH 0
-#define XVR_VERSION_BUILD Xvr_private_version_build()
 
+#define XVR_VERSION_BUILD Xvr_private_version_build()
 XVR_API const char *Xvr_private_version_build();
 
 #endif // !COMMON_XVR_TYPES_H
